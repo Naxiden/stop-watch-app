cb0c4fee3ebfbae271afda46ba8725df7f72f4a3
Naxiden <bialikandrzej10@gmail.com>
2017-10-04 18:11:25 +0200

    simple Timer app in reactjs

---
+++ stop-watch/src/Timer.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+
+class Timer extends Component {
+
+componentDidMount(){
+  this.timer = setInterval(this.ticker, 1000)
+}
+
+ticker(){
+  this.setState({clock:new Date() - this.props.start})
+}
+constructor(props){
+  super(props);
+
+  this.state = {
+    clock:0
+  };
+
+this.ticker = this.ticker.bind(this);
+}
+
+  render(){
+      let clock = Math.round(this.state.clock / 1000)
+    return(
+
+      <div>
+        <p>u have been on this site: </p> <br />
+        <span>{clock}</span>
+          <p>seconds.</p>
+      </div>
+    );
+  }
+}
+
+export default Timer;
